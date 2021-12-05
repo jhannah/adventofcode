0adef7effe0c59d25b393bb670beca749462a4a0
Jay Hannah <jay.hannah@iinteractive.com>
2021-12-05 11:25:07 -0600

    2015 Day 1

---
+++ 2015/day1.pl
@@ -0,0 +1,22 @@
+use Modern::Perl;
+use ARGV::OrDATA;
+no warnings 'experimental';
+
+my ($f, $p, $p_answer);
+while (<>) {
+  chomp;
+  foreach my $c (split //) {
+    if ($c eq "(") { $f++ }
+    if ($c eq ")") { $f-- }
+    $p++;
+    say "$p $f";
+    if ($f == -1) {
+      $p_answer //= $p;
+    }
+  }
+}
+say "Part 1: Floor: $f";
+say "Part 2: Position: $p_answer";
+
+__DATA__
+()())
