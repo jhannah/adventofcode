4827aad8b668b52be8ed915945b375603a7cd165
Jay Hannah <jay.hannah@iinteractive.com>
2021-12-07 11:06:04 -0600

    lol works, but takes 51 seconds

--- 2021/day7_part2.pl
+++ 2021/day7_part2.pl
@@ -2,23 +2,39 @@ use Modern::Perl;
 use ARGV::OrDATA;
 use List::Util qw(sum);
 
-my $fuel;
+my %pos;   # count of crabs at each position
 while (<>) {
   chomp;
-  my @c = split /,/;
-  my $final_pos = sprintf("%.0f", sum(@c) / scalar(@c));   # average, rounded
-  say "Move to $final_pos";
-  foreach my $c (@c) {
-    my $distance = abs($c - $final_pos);
-    # say "Moving $c to $final_pos";
-    foreach my $i (1..$distance) {
-      $fuel += $i;
-      # say "  + $i";
-    }
-    # say "  = $fuel";
+  foreach my $c (split /,/) {
+    $pos{$c}++;
   }
-  say "Part 2: fuel is $fuel";
 }
+
+my @all_pos = sort { $a <=> $b } keys %pos;
+my $min = $all_pos[0];
+my $max = $all_pos[-1];
+my %all_costs;
+foreach my $x ($min..$max) {
+  my $cost = cost_to_move_to($x);
+  $all_costs{$cost} = $x;
+}
+my ($min_cost) = sort { $a <=> $b } keys %all_costs;
+say "Move to $all_costs{$min_cost}, which will cost you $min_cost";
+
+
+sub cost_to_move_to {
+  (my $x) = @_;
+  my $fuel;
+  foreach my $p (keys %pos) {
+    my $distance = abs($x - $p);
+    foreach my $i (1..$distance) {
+      $fuel += $i * $pos{$p};
+    }
+  }
+  say "Moving to $x would cost $fuel";
+  return $fuel;
+}
+
 
 __DATA__
 16,1,2,0,4,2,7,1,2,14
